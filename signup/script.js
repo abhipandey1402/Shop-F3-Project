a421345e34672ca792f5b0efda41364928dbd808
Abhi <pandeyabhi142002@gmail.com>
2023-05-02 18:12:50 +0530

    Done with signup page and fixed some css errors

---
+++ signup/script.js
@@ -0,0 +1,60 @@
+
+let message = document.getElementById('message');
+let signupBtn = document.getElementById('signupBtn');
+
+
+signupBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let fName = document.getElementById('fName').value;
+    let lName = document.getElementById('lName').value;
+    let email = document.getElementById('email').value;
+    let pass = document.getElementById('pass').value;
+    let confirmPass = document.getElementById('confirmPass').value;
+
+    let exist = false;
+
+    let users = localStorage.getItem('users');
+
+    if (users != null) {
+        JSON.parse(localStorage.getItem('users')).forEach(ele => {
+            if (ele.email === email) {
+                exist = true;
+            }
+        });
+    }
+
+    if (exist === true) {
+        message.innerHTML = `<div style='color:#FF4F4F;'>User already exist, Try using new mail ID</div>`;
+        return;
+    }
+
+    if (fName == '' || lName == '' || email == '' || pass == '' || confirmPass == '') {
+        message.innerHTML = `<div style='color:#FF4F4F;'>Please fill out all fields</div>`;
+        return;
+    }
+
+    if (pass != confirmPass) {
+        message.innerHTML = `<div style='color:#FF4F4F;'>Password Mismatch! Please enter right password.</div>`;
+        return;
+    }
+
+    let name = fName + " " + lName;
+    let user = {
+        name: name,
+        email: email,
+        password: pass,
+    }
+
+    if (!users) {
+        users = [user];
+    } else {
+        users = JSON.parse(users);
+        users.push(user);
+    }
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+    location.href = '/login';
+})
+
